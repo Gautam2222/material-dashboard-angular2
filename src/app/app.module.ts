2a55bd535ff62b0d95d91e328514e89b67d2a102
Gautam kumar <mr.gautamit@gmail.com>
2023-08-31 07:47:57 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { LoginComponent } from './login/login.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   imports: [
@@ -17,10 +20,12 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    MatCheckboxModule,
   ],
   declarations: [
     AppComponent,
     AdminLayoutComponent,
+    LoginComponent
 
   ],
   providers: [],
